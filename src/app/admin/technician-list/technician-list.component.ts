bbf70e2658f0af0a1e4e48d2508f311fbd62817f
Shubhankar Dandekar <shubhankard34@gmail.com>
2018-01-31 14:14:36 +0530

    GS-9: Technician list with an Admin Panel

---
+++ src/app/admin/technician-list/technician-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technician-list',
+  templateUrl: './technician-list.component.html',
+  styleUrls: ['./technician-list.component.css']
+})
+export class TechnicianListComponent {
+
+}
